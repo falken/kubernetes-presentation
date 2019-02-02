50391650c791b7f0b5042ede9f2deb263e4ac62a
sn03848 <NelsonSteven@johndeere.com>
2019-02-02 17:35:14 -0600

    Initial Commit

---
+++ junk/allow_master_schedule.sh
@@ -0,0 +1,1 @@
+kubectl taint node master.kubernetes.local node-role.kubernetes.io/master:NoSchedule-
---
+++ junk/install_calico.sh
@@ -0,0 +1,4 @@
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
+
